cbc9fa52ac0bdc0096276443d6552c807d35fc7e
박준하 <goodjunha@gmail.com>
2024-03-30 22:29:56 +0900

    rep:: [#3] ts 변경

--- src/Auth/Login/Login.tsx
+++ src/Auth/Login/Login.tsx
@@ -1,7 +1,24 @@
+import React, { useState } from 'react';
 import './Login.css';
 import decoLogin from '../../assets/image/decoLogin.svg';
 
-function Login() {
+const Login: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLoginClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    console.log("로그인 시도:", username, password);
+  };
+
   return (
     <div className="loginWrapper">
       <div className="loginContainer">
@@ -10,15 +27,16 @@ function Login() {
         </div>
         <div className="inputFieldContainer label-id">
           <label htmlFor="username">아이디</label>
-          <input type="text" id="username" placeholder="아이디를 입력해주세요." />
+          <input type="text" id="username" placeholder="아이디를 입력해주세요." value={username} onChange={handleUsernameChange} />
         </div>
         <div className="inputFieldContainer label-password">
           <label htmlFor="password">비밀번호</label>
-          <input type="password" id="password" placeholder="비밀번호를 입력해주세요." />
+          <input type="password" id="password" placeholder="비밀번호를 입력해주세요." value={password} onChange={handlePasswordChange} />
         </div>
-        <button>Login</button>
+        <button onClick={handleLoginClick}>Login</button>
       </div>
     </div>
   );
-}
+};
+
 export default Login;
