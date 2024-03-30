bdd528503196a0b4bb9592efcd2d5cf06e7c3845
박준하 <goodjunha@gmail.com>
2024-03-30 23:58:31 +0900

    rep:: [#3] 스타일 컴포넌트 맞게 변경

--- src/Auth/Login/Login.tsx
+++ src/Auth/Login/Login.tsx
@@ -1,42 +1,135 @@
 import React, { useState } from 'react';
-import './Login.css';
+import styled from 'styled-components';
 import decoLogin from '../../assets/image/decoLogin.svg';
+import loginBackground from '/Users/junha/Desktop/Decofolio-Frontend/src/assets/image/loginBackground.svg';
+
+interface iUserForm {
+  name: string,
+  password: string
+}
 
 const Login: React.FC = () => {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [userForm, setUserForm] = useState<iUserForm>({name: '', password: ''});
 
   const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
+    setUserForm((prev) => ({
+      ...prev,
+     name: event.target.value
+    }))
   };
 
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
+    setUserForm((prev) => ({
+      ...prev,
+     password: event.target.value
+    }))
   };
 
   const handleLoginClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    console.log("로그인 시도:", username, password);
+    console.log("로그인 시도:", userForm.name, userForm.password);
   };
 
   return (
-    <div className="loginWrapper">
-      <div className="loginContainer">
-        <div className="imageContainer">
+    <LoginWrapper>
+      <LoginContainer>
+        <ImageContainer>
           <img src={decoLogin} alt="login" />
-        </div>
-        <div className="inputFieldContainer label-id">
+        </ImageContainer>
+        <InputFieldContainer className="label-id">
           <label htmlFor="username">아이디</label>
-          <input type="text" id="username" placeholder="아이디를 입력해주세요." value={username} onChange={handleUsernameChange} />
-        </div>
-        <div className="inputFieldContainer label-password">
+          <StyledInput type="text" id="username" placeholder="아이디를 입력해주세요." value={userForm.name} onChange={handleUsernameChange} />
+        </InputFieldContainer>
+        <InputFieldContainer className="label-password">
           <label htmlFor="password">비밀번호</label>
-          <input type="password" id="password" placeholder="비밀번호를 입력해주세요." value={password} onChange={handlePasswordChange} />
-        </div>
-        <button onClick={handleLoginClick}>Login</button>
-      </div>
-    </div>
+          <StyledInput type="password" id="password" placeholder="비밀번호를 입력해주세요." value={userForm.password} onChange={handlePasswordChange} />
+        </InputFieldContainer>
+        <StyledButton onClick={handleLoginClick}>Login</StyledButton>
+      </LoginContainer>
+    </LoginWrapper>
   );
 };
+
+const LoginWrapper = styled.div`
+    background-image: url(${loginBackground});
+    background-size: cover;
+    background-position: center;
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const LoginContainer = styled.div`
+    width: 600px;
+    height: 656px;
+    background-color: #FFFFFF;
+    border-radius: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 50px;
+    z-index: 1;
+    box-shadow: 0 2px 3px 2px rgba(0, 0, 0, 0.2);
+`;
+
+const ImageContainer = styled.div`
+    img {
+        width: 200px;
+        height: 100px;
+        margin-top: 60px; 
+    }
+`;
+
+const InputFieldContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    margin-top: 30px;
+
+    &.label-id label, &.label-password label {
+        align-self: flex-start;
+        color: black;
+        width: 230px;
+    }
+
+    &.label-password label {
+        width: 240px;
+    }
+`;
+
+const StyledInput = styled.input`
+    width: 400px;
+    height: 51px;
+    border-radius: 8px;
+    border: 1px solid #cccccc;
+    background-color: white;
+    padding-left: 20px;
+    color: black;
+    font-size: 16px;
+    margin-top: 8px;
+
+    &::placeholder {
+        color: #8E8E8E;
+        opacity: 1;
+        font-size: 16px;
+    }
+`;
+
+const StyledButton = styled.button`
+    width: 420px;
+    height: 51px;
+    background-color: #636363;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-top: 70px;
+`;
 
 export default Login;
