2a104ca1a3322f63b4f3c937b9a42ed1b5a80516
김철우 <chul8747@naver.com>
2024-04-28 21:32:06 +0900

    fix :: Auth 폴더를 pages로 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./Auth/Login/Login";
+import Login from "./pages/Loginpage";
 
 function App() {
   return (
